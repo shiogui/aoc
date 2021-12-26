ad5e394ec21537bc48c2a76839bc017fe0880dab
Shiogui Nagasse <shiogui@gmail.com>
2021-12-26 13:36:39 -0300

    Fim desafio 1

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,22 @@
+﻿var inputs = File.ReadAllLines("Input.txt");
+
+if (inputs == null || !inputs.Any())
+{
+    Console.WriteLine("Nothing Here!");
+    return;
+}
+
+int count = 0;
+int previous = 0;
+
+for (int i = 0; i < inputs.Length; i++)
+{
+    int v = int.Parse(inputs[i]);
+    if (previous > 0 && v > previous)
+        count++;
+    previous = v;
+}
+
+Console.WriteLine(count);
+Console.WriteLine("Finished");
+Console.ReadKey();
